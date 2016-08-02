5326b1344e19464c6245de38f89933b441216336
ravenusmc <mcuddy77@gmail.com>
2016-08-02 11:03:54 -0400

    added comments and other small modifications

--- test.py
+++ test.py
@@ -7,6 +7,32 @@ import matplotlib.pyplot as plt
 cols = ['City', 'Color', 'Shape', 'State', 'Time']
 ufo = pd.read_csv('http://bit.ly/uforeports', names=cols)
 
+#print(ufo.dtypes)
 
-ufo.Shape.value_counts().plot(kind="bar")
-plt.show()
+
+#print(ufo[(ufo.Time > "12/24/2000 18:00")])
+
+# test = ufo[ufo.Time.str.contains('1950')]
+
+# print(test.City.count())
+
+#CONTAINS LINE
+#print(ufo[ufo.Time.str.contains('1999')])
+
+year = [] 
+
+# year.append(2)
+# print(year)
+
+yr = int(input("Enter Year:"))
+print(type(yr))
+x = str(yr)
+print(type(x))
+year.append(yr)
+print(year)
+
+#I need to ask user for the year-which will be an int.
+#I then need to convert that year into a str and put it into the contains line (see above)
+#Once I have the year I need to get the count of the sighting for that year 
+#I push the count into an array 
+#All of this, or most of it, should be in a loop that adds one to the year, which will have to be an int. 
